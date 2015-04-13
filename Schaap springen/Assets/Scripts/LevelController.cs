33c6f42129b8d46d029ff936c9b6c8b653b6ded0
Nielsfcg1 <nielsfcg1@gmail.com>
2015-04-13 14:48:27 +0200

    1e commit

---
+++ Schaap springen/Assets/Scripts/LevelController.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelController: MonoBehaviour {
+	
+	public int sheepSpeed;
+	public float sheepSpawnChance;
+	public GameObject sheepPrefab;
+	
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		Vector3 sheepPosition = new Vector3();
+		
+		if(Random.value<sheepSpawnChance)
+		{
+			sheepPosition.x = 5.8f;
+			sheepPosition.y = 2.0f;
+			sheepPosition.z = -7.0f;
+			Instantiate(sheepPrefab, sheepPosition, sheepPrefab.transform.rotation);
+			
+			if(sheepPosition.z >= 6.0f)
+			{ 
+				Destroy(gameObject);
+			}
+			
+		}
+		
+		
+		
+	}
+}
---
+++ Schaap springen/Assets/Scripts/SpringScript.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class SpringScript : MonoBehaviour {
+
+	public int sheepSpeed;
+	public GameObject sheepPrefab;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		Vector3 movement = new Vector3 ();
+		movement.z = Time.deltaTime * sheepSpeed;
+		transform.Translate (movement);
+
+		Vector3 sheepPosition = new Vector3 ();
+	}
+
+		void OnTriggerEnter (Collider collider)
+		{
+			if( collider.tag == "EndGameCollider")
+			{ Destroy(gameObject);
+			print ("hit");
+			}
+		}
+
+		//if(colliding.CompareTag("EndLevelCollider"))
+		//{
+		//	Destroy(GameObject);
+		//}
+
+
+	
+
+}
